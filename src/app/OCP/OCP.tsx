c5eafb53b9e8c9e934a3146ae5c11cc82900d44f
Rebecca Alpert <ralpert@redhat.com>
2024-10-11 15:32:44 -0400

    feat(BaseChatbot): Extend Chatbot into base component
    Use props to enable other assistants - added to routes as well. Unsure if we want to handle routing this way.

--- src/app/OCP/OCP.tsx
+++ src/app/OCP/OCP.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { BaseChatbot } from '@app/BaseChatbot/BaseChatbot';
 
-const RHOAI: React.FunctionComponent = () => (
+const OCP: React.FunctionComponent = () => (
   <BaseChatbot
-    title="Red Hat OpenShift AI"
+    title="Red Hat OpenShift Container Platform"
     url="https://quarkus-llm-router-rhsaia-dev.apps.rhsaia.vg6c.p1.openshiftapps.com/assistant/chat/streaming"
-    assistantName="default_rhoai"
+    assistantName="default_ocp"
   />
 );
 
-export { RHOAI };
+export { OCP };
